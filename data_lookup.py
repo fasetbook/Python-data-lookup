e6fa39342dd1d3f801f23155fb6686624954ea2d
fasetbook <107675067+fasetbook@users.noreply.github.com>
2022-07-25 07:09:18 +0000

    Create data_lookup.py

---
+++ data_lookup.py
@@ -0,0 +1,6 @@
+depot="ham,Steak Wellington,wrench,AK47,N94，98K"
+name=input("Please enter the name of the item you are looking for:")
+if name in depot:
+  print("found the "+name+".")
+  else
+  print("Sorry no "+name+" found, request to buy!")
